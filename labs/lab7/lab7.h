77e2aab69e9e579c5b59567a4f3b8c2ac670f285
jacobsphoon <jacob9206@outlook.com>
2022-03-18 17:27:25 -0500

    lab7 	modified:   labs/lab7/Makefile 	modified:   labs/lab7/lab7.c 	modified:   labs/lab7/lab7.h 	modified:   labs/lab7/main.c

--- labs/lab7/lab7.h
+++ labs/lab7/lab7.h
@@ -1,2 +1,22 @@
+<<<<<<< HEAD
 #include<stdio.h>
 #include<stdlib.h>
+=======
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct Node Node;
+
+struct Node {
+	void *data;
+	Node *next;
+};
+
+Node * initList();
+int getSize(Node *list);
+int insertAtIndex(Node *list, void *data, int index);
+void * getAtIndex(Node *list, int index);
+void * removeAtIndex(Node *list, int index);
+void * removeAtTail(Node *list);
+void freeList(Node **list);
+>>>>>>> 0ae0bfa (lab7)
--- labs/lab7/main.c
+++ labs/lab7/main.c
@@ -1,6 +1,39 @@
 #include "lab7.h"
-
+#include<assert.h>
 int main(void)
 {
-    
+    Node * list = initList();
+    assert(list != NULL);
+    printf("Dummy Node is Located at %p\n", list);
+    int a=1, b=2, c=3, d=4,e=5;
+    assert( 0 == insertAtIndex(list, &a, 0));
+    insertAtIndex(list, &b, 0);
+    insertAtIndex(list, &c, 0);
+    insertAtIndex(list, &d, 2);
+    insertAtIndex(list, &e, getSize(list));
+    printf("Size is %d\n", getSize(list));
+    printf("debug %p->%p->%p\n", list, list->next, list->next->next);
+    for(int i = 0, *j; i< getSize(list); i++)
+    {
+        j = getAtIndex(list, i);
+        if ( j != NULL )
+        {
+            printf("Node %d = %d\n", i, *j);
+            //free(j);
+        }
+        else
+            puts("removeAtIndex(list,0) returned NULL!\n");
+    }
+    int size = getSize(list);
+    for(int i = 0, *j; i < size; i++)
+    {
+        j = removeAtTail(list);
+        if ( j != NULL )
+        {
+            printf("%d removed tail = %d\n", i, *j);
+            printf("Size is now %d\n", getSize(list));
+        }
+    }
+
+    freeList(&list);
 }
